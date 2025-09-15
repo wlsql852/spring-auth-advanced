fd1259f5ea282206a5547ee05e77c0fba017e069
wlsql852@naver.com <wlsql852@naver.com>
2025-09-15 22:49:45 +0900

    1-8 로그인 구현 jwt

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,12 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/user/login-page")
     public String loginPage() {
@@ -31,5 +31,17 @@ public class UserController {
         // 회원가입 로직 처리 (예: 서비스 호출)
         userService.signup(requestDto);
         return "redirect:/api/user/login-page"; // 회원가입 후 로그인 페이지로 리다이렉트
+    }
+
+    @PostMapping("/user/login" )
+    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+        try {
+            // 로그인 로직 처리 (예: 서비스 호출)
+            userService.login(requestDto, res);
+        } catch (Exception e) {
+            return "redirect:/api/user/login-page?error"; // 로그인 실패 시 다시 로그인 페이지로 리다이렉트  //오류날시 표시하자고 클라이언트와 약속
+        }
+
+        return "redirect:/"; // 로그인 후 메인 페이지로 리다이렉트
     }
 }
--- src/main/java/com/sparta/springauth/service/UserService.java
+++ src/main/java/com/sparta/springauth/service/UserService.java
@@ -1,24 +1,25 @@
 package com.sparta.springauth.service;
 
+import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.entity.User;
 import com.sparta.springauth.entity.UserRoleEnum;
+import com.sparta.springauth.jwt.JwtUtil;
 import com.sparta.springauth.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    private final JwtUtil jwtUtil;
 
     // ADMIN_TOKEN
     //관리자 확인 토큰
@@ -53,5 +54,24 @@ public class UserService {
         // 사용자 등록
         User user = new User(username, password, email, role);
         userRepository.save(user);
+    }
+
+    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        // 사용자 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+
+        // 비밀번호 확인
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+        // 유저 아이디와 권한으로 JWT 토큰 생성
+        String token = jwtUtil.createToken(user.getUsername(), user.getRole());
+        // JWT 토큰을 쿠키에 저장
+        jwtUtil.addJwtToCookie(token, res);
     }
 }
