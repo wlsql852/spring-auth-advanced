2d2151dc421bd66078ba482f1f123bbc7837479c
wlsql852@naver.com <wlsql852@naver.com>
2025-09-16 12:35:44 +0900

    1-11 Spring Security 로그인

--- src/main/java/com/sparta/springauth/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/springauth/config/WebSecurityConfig.java
@@ -14,19 +14,29 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        //해커가 저장된 쿠키의 세션정보로 가짜 요청을 보낼 수 있음
-        // 이를 방지하기 위해 CSRF 설정시 html에서 csrf 토큰 값을 넘겨줘야 요청 수신 가능  //그러나 사용하지 않겠다
+        // CSRF 설정
         http.csrf((csrf) -> csrf.disable());
 
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정(css, js 등)
-                        .requestMatchers("/api/user/**").permitAll() // 회원가입, 로그인 API 는 인증처리 하지 않음  //permitAll()뿐만 아니라 다양한 조건을 즐 수 있음
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                        .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
         // 로그인 사용
-        http.formLogin(Customizer.withDefaults());
+        http.formLogin((formLogin) ->
+                formLogin
+                        // 로그인 View 제공 (GET /api/user/login-page)
+                        .loginPage("/api/user/login-page")
+                        // 로그인 처리 (POST /api/user/login)
+                        .loginProcessingUrl("/api/user/login")
+                        // 로그인 처리 후 성공 시 URL
+                        .defaultSuccessUrl("/")
+                        // 로그인 처리 후 실패 시 URL
+                        .failureUrl("/api/user/login-page?error")
+                        .permitAll()
+        );
 
         return http.build();
     }
